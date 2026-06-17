6318df06dc1e64a9fcf6dffbb39907515399b565
havok <havokzeroz@gmail.com>
2026-06-17 11:58:27 -0700

    fix: patch unix build gate and grant GH actions write permissions

--- build.rs
+++ build.rs
@@ -1,12 +1,16 @@
 use std::io;
 
+// This function ONLY exists when compiling on Windows
+#[cfg(windows)]
 fn main() -> io::Result<()> {
-    // Only run this resource compiler if we are building for Windows
-    if std::env::var("CARGO_CFG_TARGET_OS").unwrap() == "windows" {
-        let mut res = winres::WindowsResource::new();
-        // Point this to your generated icon
-        res.set_icon("assets/zdl-echo.ico");
-        res.compile()?;
-    }
+    let mut res = winres::WindowsResource::new();
+    res.set_icon("assets/zdl-echo.ico");
+    res.compile()?;
+    Ok(())
+}
+
+// This function ONLY exists when compiling on Mac or Linux
+#[cfg(not(windows))]
+fn main() -> io::Result<()> {
     Ok(())
 }
